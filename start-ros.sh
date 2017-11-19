0fd4b9676b461fb25e67a8330b997de4d0ef3899
lkm1321 <lkm1321@gmail.com>
2017-11-20 03:08:14 +1100

    Shakedown

--- start-ros.sh
+++ start-ros.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker rm intel-aero-docker
 docker run --privileged --name=intel-aero-docker --net=host -u=root --device=/dev/dri:/dev/dri --entrypoint=/ros_entrypoint_standalone.sh lkm1321/intel-aero-acfr:latest roslaunch intel-aero-acfr intel-aero-acfr.launch >> /log.txt
 
